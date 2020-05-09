1c149a73d90dfc1e97d2d79701d1929882bd25a4
El Tucker <elchristopher@outlook.com>
2020-05-09 00:06:59 -0400

    learned states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Tweet from './Tweet';
 
 function App() {
+
+const [isRed, setRed] = useState(false);
+const [count, setCount] = useState(0);
+
+const increment = () => {
+  setCount(count + 1);
+}
+
   return (
     <div className="app">
-      <h1>Hello React</h1>
-      <Tweet 
-      name = "Maria"/>
-      <Tweet />
-      <Tweet />
-      <Tweet />
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
     </div>
   )
 }
