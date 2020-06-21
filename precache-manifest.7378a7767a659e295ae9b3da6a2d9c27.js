0a3daff618647b986d7aa7ebad2ebb173b969164
El'Christopher Tucker <elchristopher@outlook.com>
2020-06-21 00:45:19 -0400

    Updates

---
+++ precache-manifest.7378a7767a659e295ae9b3da6a2d9c27.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600586dadd5b82098cdf5ecdd61301cc",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "8f5e1e241ef63dba2a76",
+    "url": "/portfolio-website/static/css/2.2effce0f.chunk.css"
+  },
+  {
+    "revision": "a1c8f91d0bddcffe4486",
+    "url": "/portfolio-website/static/css/main.91f6188c.chunk.css"
+  },
+  {
+    "revision": "8f5e1e241ef63dba2a76",
+    "url": "/portfolio-website/static/js/2.510f7d0e.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/portfolio-website/static/js/2.510f7d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c8f91d0bddcffe4486",
+    "url": "/portfolio-website/static/js/main.d530afa9.chunk.js"
+  },
+  {
+    "revision": "8fb13005fd53af59c854",
+    "url": "/portfolio-website/static/js/runtime-main.e7f481cf.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/portfolio-website/static/media/GitHub_Logo.cefc2023.png"
+  },
+  {
+    "revision": "5b5547f7411e2b50b71665253e2930c9",
+    "url": "/portfolio-website/static/media/IMG_0816.5b5547f7.PNG"
+  },
+  {
+    "revision": "c24cde2827448f08d40afd6909011e16",
+    "url": "/portfolio-website/static/media/LI-Logo.c24cde28.png"
+  },
+  {
+    "revision": "41e844ba6f439521e87dc49f3ac7d050",
+    "url": "/portfolio-website/static/media/WHNGIF1.41e844ba.gif"
+  },
+  {
+    "revision": "55bd843fd19c414a0978b99bfc30a5f4",
+    "url": "/portfolio-website/static/media/WHNGIF2.55bd843f.gif"
+  },
+  {
+    "revision": "09155d1e16adeb050960addab4575433",
+    "url": "/portfolio-website/static/media/WHNGIF3.09155d1e.gif"
+  },
+  {
+    "revision": "8b0ae999dc589a6e2f20b654c048a067",
+    "url": "/portfolio-website/static/media/WHNSS1.8b0ae999.png"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/portfolio-website/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/portfolio-website/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/portfolio-website/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/portfolio-website/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  },
+  {
+    "revision": "db18fa1b919dc93edb5083dbd885f848",
+    "url": "/portfolio-website/static/media/the holy sword images.db18fa1b.png"
+  },
+  {
+    "revision": "96158d79884111425faedb2a0efc48fd",
+    "url": "/portfolio-website/static/media/thsss2.96158d79.png"
+  },
+  {
+    "revision": "f0c6183e14805502afa760c6560ae949",
+    "url": "/portfolio-website/static/media/thsss3.f0c6183e.png"
+  }
+]);
